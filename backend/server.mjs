9e147b78fc890454bacc6ae8ade4a052c6f0eac2
Sathish <rhbsk1994@gmail.com>
2025-02-22 09:06:26 +0530

    Fixed CORS issue - added netlify url to cors

--- backend/server.mjs
+++ backend/server.mjs
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: '*', // ✅ Replace with your frontend URL,'http://localhost:5173'
+    origin: "https://boomerv1.netlify.app", // ✅ Replace with your frontend URL,'http://localhost:5173'
     credentials: true // ✅ Allows cookies to be sent
 }));
 
